37965e6e77a20452e3bcd4e23b8759bbe63c9c7d
ana-bharadwaj <80563096+ana-bharadwaj@users.noreply.github.com>
2025-03-26 18:06:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+function reverseString() {
+  const input = document.getElementById('firstString').value;
+  const reversed = input.split('').reverse().join('');
+  document.getElementById('output').textContent = 'Reversed: ' + reversed;
+}
+function palindromeNumber() {
+  let num = parseInt(document.getElementById('number').value);
+  let numCopy = num;
+  let index = 0;
+  let num2 = 0;
+  while (numCopy > 0) {
+    let digit = numCopy % 10;
+    num2 = 10 * num2 + digit;
+    numCopy = parseInt(numCopy / 10);
+    index++;
+  }
+
+  if (num2 == num) {
+    document.getElementById('secondAnswer').innerHTML =
+      'The number entered is a palindrome number';
+  } else {
+    document.getElementById('secondAnswer').innerHTML =
+      'The number entered is not a palindrome number';
+  }
+}
+document
+  .getElementById('numberBtn')
+  .addEventListener('click', palindromeNumber);
+
+function restaurant() {
+  let subtotalAmt = parseFloat(
+    document.getElementById('subtotal').value
+  ).toFixed(2);
+  let tipPercent = parseFloat(document.getElementById('tip').value).toFixed(2);
+  if (tipPercent > 100) tipPercent = 100;
+  let totalAmt = subtotalAmt * (1 + tipPercent / 100);
+  document.getElementById('thirdAnswer').innerHTML =
+    'The total amount to be paid to the restaurant : ' + totalAmt.toFixed(2);
+}
+document.getElementById('subtotalBtn').addEventListener('click', restaurant);
